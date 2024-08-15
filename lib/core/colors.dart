2de112502894847960c637edb09e79abdab9b5c6
AMAN-KANHIKOTH <amankanhikoth@gmail.com>
2024-08-15 13:11:47 +0530

    Spotify Project Started

---
+++ lib/core/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const BottomNavigationSelected = Colors.white;
+const BottomNavigationUnselected = Colors.grey;
---
+++ lib/presentation/main_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_clone/core/colors.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // bottomNavigationBar: BottomNavigationBar(
+        //   items: const [
+        //     BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        //     BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+        //     BottomNavigationBarItem(
+        //         icon: Icon(Icons.library_music), label: 'Your Library'),
+        //   ],
+        //   selectedItemColor: BottomNavigationSelected,
+        // ),
+        );
+  }
+}
