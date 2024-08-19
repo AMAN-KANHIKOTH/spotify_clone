bf30657edbc1ba25e6e4c1913faa0bf0e72ee29f
AMAN-KANHIKOTH <amankanhikoth@gmail.com>
2024-08-19 14:32:37 +0530

    your favourite artists displayed with a sample artists

--- lib/presentation/home/widgets/your_favourite_artists.dart
+++ lib/presentation/home/widgets/your_favourite_artists.dart
@@ -1,5 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spotify_clone/application/home/bloc/home_bloc.dart';
 
 class YourFavouriteArtists extends StatelessWidget {
   const YourFavouriteArtists({super.key});
@@ -20,27 +24,39 @@ class YourFavouriteArtists extends StatelessWidget {
         const SizedBox(height: 5),
         SizedBox(
           height: size.height * 0.225,
-          child: ListView(
-            shrinkWrap: true,
-            scrollDirection: Axis.horizontal,
-            children: List.generate(
-              5,
-              (index) => Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8),
-                child: Column(
-                  children: [
-                    CircleAvatar(
-                      minRadius: 80,
+          child: BlocBuilder<HomeBloc, HomeState>(
+            builder: (context, state) {
+              log(state.artistsList[0].images![1].url.toString());
+              return ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                children: List.generate(
+                  state.artistsList.length,
+                  (index) => Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: Column(
+                      children: [
+                        CircleAvatar(
+                          minRadius: 80,
+                          backgroundImage: NetworkImage(
+                              state.artistsList[index].images![0].url!),
+                          // child: Image.network(
+                          //   fit: BoxFit.scaleDown,
+                          //   state.artistsList[index].images![0].url!,
+                          //   width: 160,
+                          // ),
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          state.artistsList[index].name ?? 'Unknown',
+                          style: TextStyle(color: Colors.white),
+                        )
+                      ],
                     ),
-                    const SizedBox(height: 5),
-                    Text(
-                      'Artist Name',
-                      style: TextStyle(color: Colors.white),
-                    )
-                  ],
+                  ),
                 ),
-              ),
-            ),
+              );
+            },
           ),
         )
       ],
